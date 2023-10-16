9dc91f3da9cb168d1403f58c39e46d9c98fa6790
Bekzat-Chauvbayev <120590181+Bekzat-Chauvbayev@users.noreply.github.com>
2023-10-16 14:34:51 +0600

    Create MainActivity.kt

---
+++ src/main/java/com/bita/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.bita.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bita.myapplication.ui.theme.ComposeArticleTheme
+import com.example.myapplication.R
+
+class MainActivity : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme{
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    ComposeArticleApp()
+                }
+
+            }
+        }
+    }
+    @Composable
+    fun ComposeArticleApp() {
+        ArticleCard(
+            title = stringResource(R.string.app_name),
+            shortDescription = stringResource(R.string.desc),
+            longDesc = stringResource(R.string.longD),
+            imagePainter = painterResource(R.drawable.bg_compose_background)
+        )
+    }
+
+    @Composable
+    private fun ArticleCard(
+        title:String,
+        shortDescription: String,
+        longDesc: String,
+        imagePainter: Painter,
+        modifier: Modifier = Modifier,
+
+    ){
+        Column(modifier = modifier){
+            Image(painter = imagePainter, contentDescription = null)
+            Text(text = title, modifier = Modifier.padding(16.dp),
+                fontSize = 24.sp)
+            Text(text = shortDescription, modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify)
+            Text(text = longDesc, modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify)
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ComposeArticleAppPreview() {
+        ComposeArticleTheme {
+            ComposeArticleApp()
+        }
+    }
+}
